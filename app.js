8f4a46373922debb32160941b4d83a21ab15f9f7
Casey Leonard <casey@amphibian.com>
2014-04-21 23:26:29 -0400

    move database access to a separate module

--- app.js
+++ app.js
@@ -1,114 +1,12 @@
 var express = require('express');
-var mysql = require('mysql');
 var fs = require('fs');
 var compress = require('compression')();
+var cdata = require('./data');
 
 var app = express();
 
 var dbconf = JSON.parse(fs.readFileSync('data/dbconf.json', { encoding: 'utf-8' }));
-
-var conn = mysql.createConnection(dbconf);
-
-conn.connect();
-
-function prevComic(d, cb) {
-	
-	var sql = 'SELECT id, DATE_FORMAT(pub_date, \'%W, %e %M %Y\') as pd, data FROM comic_data ' +
-		'WHERE pub_date = (SELECT MAX(pub_date) FROM comic_data WHERE pub_date < ?)';
-	conn.query(sql, [d], function(err, rows) {
-		
-		if (err) {
-			console.log(err);
-			cb(null);
-		} else {
-		
-			if (rows.length > 0) {
-				cb(rows[0].id);
-			} else {
-				cb(null);
-			}
-			
-		}
-		
-	});
-	
-}
-
-function nextComic(d, cb) {
-	
-	var sql = 'SELECT id, DATE_FORMAT(pub_date, \'%W, %e %M %Y\') as pd, data FROM comic_data ' +
-		'WHERE pub_date = (SELECT MIN(pub_date) FROM comic_data WHERE pub_date > ?)';
-	conn.query(sql, [d], function(err, rows) {
-		
-		if (err) {
-			console.log(err);
-			cb(null);
-		} else {
-		
-			if (rows.length > 0) {
-				cb(rows[0].id);
-			} else {
-				cb(null);
-			}
-			
-		}
-		
-	});
-	
-}
-
-function getComicFromDB(sql, req, res) {
-	
-	conn.query(sql, function(err, rows) {
-		
-		if (err) {
-			
-			res.send(500, 'error loading comic');
-			
-		} else {
-			
-			if (rows.length > 0) {
-				
-				var obj = JSON.parse(rows[0].data);
-				obj.pubDate = rows[0].pd;
-				
-				prevComic(rows[0].pub_date, function(p) {
-					
-					obj.prevDate = p;
-					
-					nextComic(rows[0].pub_date, function(n) {
-
-						obj.nextDate = n;
-						res.render('comic', obj);
-						
-					});
-					
-					
-				});
-				
-			} else {
-				
-				fs.readFile('data/404.json', { encoding: 'utf-8' }, function(err, data) {
-					if (err) {
-						res.send(500, "error getting the error comic! we gots problems!");
-					} else {
-						res.render('comic', JSON.parse(data), function(err, str) {
-							if (err) {
-								return req.next(err);
-							}
-							res.send(404, str);
-						});
-					}
-				});
-				
-			}
-			
-		}
-		
-	});
-
-}
-
+var cfact = cdata(dbconf);
 
 app.set('view engine', 'jade');
 
@@ -121,34 +19,67 @@ app.use(function(req, res, next) {
 app.use(compress);
 
 // if nothing explicit requested, send most recent comic
-app.get('/', function(req, res) {
+app.get('/', function(req, res, next) {
 	
-	var sql = 'SELECT pub_date, DATE_FORMAT(pub_date, \'%W, %e %M %Y\') as pd, data FROM comic_data ' +
-		'WHERE pub_date = (SELECT MAX(pub_date) FROM comic_data)';
-	getComicFromDB(sql, req, res);
-	
-});
-
-app.get('/:n', function(req, res) {
-
-	var sql = 'SELECT pub_date, DATE_FORMAT(pub_date, \'%W, %e %M %Y\') as pd, data ' +
-		'FROM comic_data WHERE id = ' + conn.escape(req.params.n);
-	getComicFromDB(sql, req, res);
-	
-});
-
-app.get('/images/:img', function(req, res) {
-	
-	conn.query('SELECT data, type FROM comic_img WHERE filename = ?', [req.params.img], function(err, rows) {
+	cfact.loadCurrent(function (err, data) {
 		if (err) {
-			res.send(500);
+			next(err);
+		} else if (data) {
+			res.render('comicpage', data);
 		} else {
-			if (rows.length > 0) {
-				res.setHeader('Content-Type', rows[0].type);
-				res.send(new Buffer(rows[0].data));
-			} else {
-				res.send(404);
-			}
+			next(); // no comic found
+		}
+	});
+	
+	
+});
+
+// load individual comic pages by id
+app.get('/:n', function(req, res, next) {
+
+	cfact.loadById(req.params.n, function (err, data) {
+		if (err) {
+			next(err);
+		} else if (data) {
+			res.render('comicpage', data);
+		} else {
+			next(); // no comic found
+		}
+	});
+	
+});
+
+//get just the comic HTML by id
+app.get('/chtml/:n', function(req, res, next) {
+
+	cfact.loadById(req.params.n, function (err, data) {
+		if (err) {
+			next(err);
+		} else if (data) {
+			res.render('comic', data, function(err, str) {
+				if (err) {
+					next(err);
+				} else {
+					res.send(str);
+				}
+			});
+		} else {
+			next(); // no comic found
+		}
+	});
+	
+});
+
+app.get('/images/:img', function(req, res, next) {
+	
+	cfact.loadImage(req.params.img, function (err, data) {
+		if (err) {
+			next(err);
+		} else if (data) {
+			res.setHeader('Content-Type', data.contentType);
+			res.send(data.buffer);
+		} else {
+			res.send(404); // don't use the full-page 404 for missing images
 		}
 	});
 	
@@ -156,6 +87,24 @@ app.get('/images/:img', function(req, res) {
 
 app.use(express.static('public'));
 
+// handle 404
+app.use(function(req, res, next){
+	fs.readFile('data/404.json', { encoding: 'utf-8' }, function(err, data) {
+		if (err) {
+			next(err);
+		} else {
+			res.render('comic', JSON.parse(data), function(err, str) {
+				if (err) {
+					next(err);
+				} else {
+					res.send(404, str);
+				}
+			});
+		}
+	});
+});
+
+// handle 500
 app.use(function(err, req, res, next) {
 	console.error(err.stack);
 	res.send(500, 'Something broke!');
---
+++ data.js
@@ -0,0 +1,159 @@
+var mysql = require('mysql');
+
+module.exports = function(dbconf) {
+	
+	var conn = mysql.createConnection(dbconf);
+	conn.connect();
+	
+	function prevComic(d, cb) {
+		
+		var sql = 'SELECT id, DATE_FORMAT(pub_date, \'%W, %e %M %Y\') as pd, data FROM comic_data ' +
+			'WHERE pub_date = (SELECT MAX(pub_date) FROM comic_data WHERE pub_date < ?)';
+		conn.query(sql, [d], function(err, rows) {
+			
+			if (err) {
+				cb(err);
+			} else {
+				if (rows.length > 0) {
+					cb(null, rows[0].id);
+				} else {
+					cb(null, null);
+				}
+			}
+			
+		});
+		
+	}
+	
+	function nextComic(d, cb) {
+		
+		var sql = 'SELECT id, DATE_FORMAT(pub_date, \'%W, %e %M %Y\') as pd, data FROM comic_data ' +
+			'WHERE pub_date = (SELECT MIN(pub_date) FROM comic_data WHERE pub_date > ?)';
+		conn.query(sql, [d], function(err, rows) {
+			
+			if (err) {
+				cb(err);
+			} else {
+			
+				if (rows.length > 0) {
+					cb(null, rows[0].id);
+				} else {
+					cb(null, null);
+				}
+				
+			}
+			
+		});
+		
+	}
+	
+	return {
+		
+		loadCurrent: function (cb) {
+			
+			var sql = 'SELECT pub_date, DATE_FORMAT(pub_date, \'%W, %e %M %Y\') as pd, data FROM comic_data ' +
+				'WHERE pub_date = (SELECT MAX(pub_date) FROM comic_data)';
+			conn.query(sql, function(err, rows) {
+				
+				if (err) {
+					cb(err);
+				} else {
+					
+					if (rows.length > 0) {
+						var obj = JSON.parse(rows[0].data);
+						obj.pubDate = rows[0].pd;
+						
+						prevComic(rows[0].pub_date, function(err, p) {
+							
+							if (err) {
+								cb(err);
+							} else {
+								obj.prevDate = p;
+								nextComic(rows[0].pub_date, function(err, n) {
+									if (err) {
+										cb(err);
+									} else {
+										obj.nextDate = n;
+										cb(null, obj);
+									}
+								});
+								
+							}
+							
+						});
+						
+					} else {
+						cb(null, null);
+					}
+					
+				}
+
+			});
+			
+		},
+
+		loadById: function (id, cb) {
+
+			var sql = 'SELECT pub_date, DATE_FORMAT(pub_date, \'%W, %e %M %Y\') as pd, data ' +
+				'FROM comic_data WHERE id = ' + conn.escape(id);
+			conn.query(sql, function(err, rows) {
+
+				if (err) {
+					cb(err);
+				} else {
+
+					if (rows.length > 0) {
+						var obj = JSON.parse(rows[0].data);
+						obj.pubDate = rows[0].pd;
+
+						prevComic(rows[0].pub_date, function(err, p) {
+
+							if (err) {
+								cb(err);
+							} else {
+								obj.prevDate = p;
+								nextComic(rows[0].pub_date, function(err, n) {
+									if (err) {
+										cb(err);
+									} else {
+										obj.nextDate = n;
+										cb(null, obj);
+									}
+								});
+
+							}
+
+						});
+
+					} else {
+						cb(null, null);
+					}
+
+				}
+
+			});
+
+		},
+		
+		loadImage: function(name, cb) {
+
+			conn.query('SELECT data, type FROM comic_img WHERE filename = ?', [name], function(err, rows) {
+				if (err) {
+					cb(err);
+				} else {
+					if (rows.length > 0) {
+						cb(null, {
+							contentType: rows[0].type,
+							buffer: new Buffer(rows[0].data)
+						});
+					} else {
+						cb(null, null);
+					}
+				}
+			});
+
+		}
+
+	};
+	
+};
