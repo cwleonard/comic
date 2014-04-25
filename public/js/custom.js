244ddd842eb4f96d757f2460df481d90a147bcd4
Casey Leonard <casey@amphibian.com>
2014-04-24 21:53:00 -0400

    add movement to images

--- public/js/custom.js
+++ public/js/custom.js
@@ -37,3 +37,49 @@ $(function() {
     s.attr('href','mailto:' + e);
 
 });
+
+$(function() {
+	
+	var lastTime = null;
+	var counter1 = 0;
+	var counter2 = 0;
+	var direction = 1;
+	
+	function run(timestamp) {
+
+		var elapsed;
+		if (lastTime === null) {
+			lastTime = timestamp;
+		}
+		elapsed = timestamp - lastTime;
+		lastTime = timestamp;
+		
+		move = false;
+		counter1 += elapsed;
+		counter2 += elapsed;
+		if (counter1 >= 100) {
+			counter = 0;
+			move = true;
+		}
+		if (counter2 >= 1500) {
+			counter2 = 0;
+			direction *= -1;
+		}
+		
+		if (move) {
+			$("img[move='true'").each(function(idx, elem) {
+				var l = $(elem).css('left');
+				var l2 = Number(l.replace('px', ''));
+				$(elem).css('left', (l2-direction) + 'px');
+			});
+		}
+		
+		var me = this;
+		window.requestAnimationFrame(function(e) { run(e); });
+		
+	}
+	
+	var me = this;
+	window.requestAnimationFrame(function(e) { run(e); });
+	
+});
