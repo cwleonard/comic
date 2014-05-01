00983367cc7a939c5467d97b785e0070abbae2e4
Casey Leonard <casey@amphibian.com>
2014-04-30 22:56:25 -0400

    create ability to add, resize, and move images in editor

--- public/js/editor.js
+++ public/js/editor.js
@@ -19,7 +19,17 @@ function doSave2() {
 	};
 	$('#comicArea div.box').each(function(idx, elem) {
 		console.log('found box element');
-		cobj.cells.push({});
+		var b = {imgs: []};
+		$(elem).find(".divimg").each(function(idx, elem) {
+			console.log('found image element');
+			var t = $(elem).css('top');
+			var l = $(elem).css('left');
+			b.imgs.push({
+				top: t,
+				left: l
+			});
+		});
+		cobj.cells.push(b);
 	});
 	
 	$('#data').val(JSON.stringify(cobj));
@@ -28,6 +38,35 @@ function doSave2() {
 
 function addCell() {
 
-	$('#comicArea').prepend("<div class='box'></div>");
+	$('#comicArea').append("<div class='box'></div>");
 
 }
+
+$(function() {
+	
+	$.contextMenu({
+		
+		selector: '.box',
+		items: {
+			bgcolor: {
+				name: 'Background',
+				type: 'text',
+				value: '#FFFFFF'
+			},
+			sep1: "----------",
+			addImage: {
+				name: "Add Image",
+				callback: function(key, options) {
+					console.log(key);
+					// see http://stackoverflow.com/questions/4948582/jquery-draggable-and-resizable
+					$(options.$trigger).append("<div class='divimg'><img style='height: 100px; width: 100px;' src='/images/frog.svg'/></div>");
+					$('.box > .divimg').draggable();
+					$('.box > .divimg > img').resizable({aspectRatio: true});
+
+				}
+			}
+		}
+		
+	});
+	
+});
