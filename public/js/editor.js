3d6f3d243103a2aa5125aa62ea5cf4287e2619d5
Casey Leonard <casey@amphibian.com>
2014-05-01 23:17:22 -0400

    add speech bubble editing

--- public/js/editor.js
+++ public/js/editor.js
@@ -18,27 +18,60 @@ function doSave2() {
 		cells: []
 	};
 	$('#comicArea div.box').each(function(idx, elem) {
+		
 		console.log('found box element');
-		var b = {imgs: []};
+		var b = {
+			bubble: undefined,
+			imgs: []
+		};
+		
+		$(elem).find("p.bubble").each(function(idx, elem) {
+			var t = $(elem).css('top');
+			var l = $(elem).css('left');
+			var w = $(elem).css('width');
+			$(elem).find("span").each(function(idx, elem) {
+				b.bubble = {
+						top: t,
+						left: l,
+						width: w,
+						text: $(elem).text()
+				};
+			});
+		});
+		
 		$(elem).find(".divimg").each(function(idx, elem) {
 			console.log('found image element');
 			var t = $(elem).css('top');
 			var l = $(elem).css('left');
-			b.imgs.push({
-				top: t,
-				left: l
+			$(elem).find("div.ui-wrapper").each(function(idx, elem) {
+				var w = $(elem).css('width');
+				$(elem).find("img").each(function(idx, elem) {
+					var s = elem.src;
+					s = s.substring(s.lastIndexOf('/')+1);
+					b.imgs.push({
+						top: t,
+						left: l,
+						width: w,
+						src: s
+					});
+				});
 			});
 		});
+		
 		cobj.cells.push(b);
+		
 	});
 	
-	$('#data').val(JSON.stringify(cobj));
+	//$('#data').val(JSON.stringify(cobj));
+	console.log(cobj);
 
 }
 
 function addCell() {
 
-	$('#comicArea').append("<div class='box'></div>");
+	var d = document.createElement('div');
+	$(d).addClass('box');
+	$('#comicArea').append(d);
 
 }
 
@@ -58,13 +91,48 @@ $(function() {
 				name: "Add Image",
 				callback: function(key, options) {
 					console.log(key);
+					
+					var d = document.createElement('div');
+					var i = document.createElement('img');
+					
+					$(i).css('height','100px');
+					$(i).css('width','100px');
+					$(i).attr('src', '/images/frog.svg');
+					
+					$(d).addClass('divimg');
+					$(d).append(i);
+					
 					// see http://stackoverflow.com/questions/4948582/jquery-draggable-and-resizable
-					$(options.$trigger).append("<div class='divimg'><img style='height: 100px; width: 100px;' src='/images/frog.svg'/></div>");
-					$('.box > .divimg').draggable();
-					$('.box > .divimg > img').resizable({aspectRatio: true});
+					$(options.$trigger).append(d);
+					
+					$(d).draggable();
+					$(i).resizable({aspectRatio: true});
 
 				}
+			},
+			addBubble: {
+				name: "Add Bubble",
+				callback: function(key, options) {
+					console.log(key);
+					var p = document.createElement('p');
+					var s = document.createElement('span');
+					$(s).append(document.createTextNode('whatnot'));
+					
+					$(p).addClass('bubble');
+					$(p).append(s);
+					
+					$(options.$trigger).append(p);
+					$(p).draggable().resizable();
+					$(s).editable({
+						type: 'text',
+						mode: 'inline',
+						success: function(response, newValue) {
+							console.log(newValue);
+						}
+					});
+				}
 			}
+
 		}
 		
 	});
