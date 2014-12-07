76986a54662aa1a5cb324a5099af15cc0db87e2b
Casey Leonard <casey@amphibian.com>
2014-12-06 20:52:42 -0500

    i don't like xCharts anymore, switching to Chart.js

--- public/js/stats.js
+++ public/js/stats.js
@@ -1,40 +1,108 @@
-function doDayChart() {
+function doDayChart(numDays) {
 
-	$.get('/stats/viewsByDay/7', function(data) {
+	var dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+	var monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+	
+	$.get('/stats/viewsByDay/' + numDays, function(data) {
 		
-		var cdat = [];
+		var labels = [];
+		var values = [];
 		for (d in data) {
-			cdat.push({
-				"x": data[d].day,
-				"y": data[d].count
-			});
+			
+			var l = "";
+			if (numDays > 7) {
+				var theDate = new Date(data[d].day);
+				l = monthNames[theDate.getMonth()] + " " + theDate.getDate();
+			} else {
+				l = dayNames[(new Date(data[d].day)).getDay()];
+			}
+			
+			labels.push(l);
+			values.push(data[d].count);
 		}
 		
 		var chartData = {
-			"xScale": "time",
-			"yScale": "linear",
-			"type": "line",
-			"main": [{
-				"className": ".comicsAccessed",
-				"data": cdat
-			}]
-		};
+			    "labels": labels,
+			    datasets: [
+			        {
+			            label: "Comic Views",
+			            fillColor: "rgba(151,187,205,0.2)",
+			            strokeColor: "rgba(0,51,102,1)",
+			            pointColor: "rgba(151,187,205,1)",
+			            pointStrokeColor: "#fff",
+			            pointHighlightFill: "#fff",
+			            pointHighlightStroke: "rgba(220,220,220,1)",
+			            data: values
+			        }
+			    ]
+			};		
 		
 		console.log(chartData);
 		
 		var opts = {
-				  "dataFormatX": function (x) { return new Date(x); },
-				  "tickFormatX": function (x) { return d3.time.format('%A')(x); }
-				};
-
-		var myChart = new xChart('line', chartData, '#chart1', opts);
+			"scaleGridLineColor": "rgba(0,0,0,.08)",
+			"datasetStrokeWidth": 3,
+			"responsive": true
+		};
+		
+		var ctx = document.getElementById("viewsChart").getContext("2d");
+		var myChart = new Chart(ctx).Line(chartData, opts);
 
 	}, 'json');
 
 }
 
+function doAgentChart(numDays) {
+
+	var hours = numDays * 24;
+	
+	$.get('/stats/agents/' + hours, function(data) {
+		
+
+		var chartData = [ {
+			value : data.firefox,
+			color : "#F7464A",
+			highlight : "#FF5A5E",
+			label : "Firefox"
+		}, {
+			value : data.chrome,
+			color : "#46BFBD",
+			highlight : "#5AD3D1",
+			label : "Chrome"
+		}, {
+			value : data.ie,
+			color : "#FDB45C",
+			highlight : "#FFC870",
+			label : "IE"
+		}, {
+			value : data.safari,
+			color : "#FDB45C",
+			highlight : "#FFC870",
+			label : "Safari"
+		}, {
+			value : data.other,
+			color : "#FD045C",
+			highlight : "#FF9870",
+			label : "Other"
+		} ];
+		
+		console.log(chartData);
+		
+		var opts = {
+			"responsive": true
+		};
+		
+		var ctx = document.getElementById("agentsChart").getContext("2d");
+		var myChart = new Chart(ctx).Pie(chartData, opts);
+
+	}, 'json');
+
+}
+
+
 $(function() {
 	
-	doDayChart();
+	doDayChart(7);
+	doAgentChart(7);
 	
 });
