2cd642ede4fe0b50dd600ee0367d9645e2380ddd
Casey Leonard <casey@amphibian.com>
2015-05-09 09:23:13 -0400

    javascript for upcoming comic

---
+++ public/js/infinity.js
@@ -0,0 +1,11 @@
+$(function() {
+
+	var pubDate = new Date("11 May 2015");
+	var nowDate = new Date();
+	
+	var startNumber = ((nowDate - pubDate) / 1000) / 5;
+	
+	console.log(startNumber);
+	
+	
+});
