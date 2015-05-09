dadd6b3c3c00fd46e2ebe3e22755eb7e2bbd720f
Casey Leonard <casey@amphibian.com>
2015-05-09 09:24:20 -0400

    update javascript for upcoming comic

--- public/js/infinity.js
+++ public/js/infinity.js
@@ -3,9 +3,20 @@ $(function() {
 	var pubDate = new Date("11 May 2015");
 	var nowDate = new Date();
 	
-	var startNumber = ((nowDate - pubDate) / 1000) / 5;
+	var frogsHired = Math.floor(((nowDate - pubDate) / 1000) / 5);
 	
-	console.log(startNumber);
+	
+	// 9007199254740992
+	
+	var hireFrog = function() {
+		
+		$('#hiredSoFar').html(frogsHired);
+		frogsHired++;
+		setTimeout(hireFrog, 5000);
+		
+	};
+	
+	hireFrog();
 	
 	
 });
