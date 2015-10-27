c4f7176773d07572a8b304106f0b63752c583a08
Casey Leonard <casey@amphibian.com>
2015-10-26 21:19:40 -0400

    update pumpkin glow

--- public/js/pumpkin.js
+++ public/js/pumpkin.js
@@ -2,17 +2,21 @@ $(function() {
 
 	var light = function() {
 		
-	    $('#p-light').animate({backgroundColor: '#ffff66'}, 2000);
+	    $('#p-light-1').animate({backgroundColor: '#ffff66'}, 1800);
+        $('#p-light-2').animate({backgroundColor: '#ffff66'}, 2000);
+        $('#p-light-3').animate({backgroundColor: '#ffff66'}, 2200);
 	    
-		setTimeout(dark, 3000);
+		setTimeout(dark, 2400);
 		
 	};
 
    var dark = function() {
 	        
-        $('#p-light').animate({backgroundColor: '#000000'}, 2000);
+        $('#p-light-1').animate({backgroundColor: '#000000'}, 1800);
+        $('#p-light-2').animate({backgroundColor: '#000000'}, 2000);
+        $('#p-light-3').animate({backgroundColor: '#000000'}, 2200);
 	        
-        setTimeout(light, 3000);
+        setTimeout(light, 2400);
 	        
     };
 
