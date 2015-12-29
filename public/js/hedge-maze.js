5d74e9cf9e234e9510c3bac00071ab7ad32b50ce
Casey Leonard <casey@amphibian.com>
2015-12-29 15:22:48 -0500

    more hedge maze fun

--- public/js/hedge-maze.js
+++ public/js/hedge-maze.js
@@ -1,10 +1,20 @@
 
-
+var coinsCollected = 0;
+var startTime = null;
+var elapsedTime = null;
 
 function gameState() {
 	
 	var frog;
 	var layer;
+	var coins, g;
+	var coinCount, timerText;
+	
+	function collectCoin(f, c) {
+	    coinsCollected++;
+	    c.exists = false;
+	    coinCount.text = "$ " + coinsCollected;
+	}
 	
     function preload() {
         this.load.pack("main", "other/hedge-maze-pack.json");
@@ -25,8 +35,20 @@ function gameState() {
         map.setLayer(layer);
         
         map.setCollisionBetween(1, 16);
+        
+        coins = this.add.group();
+        coins.enableBody = true;
+        map.createFromObjects('coins', 17, 'coin', 0, true, false, coins);
+        
+        coins.callAll('animations.add', 'animations', 'shine', [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6], 8, true);
+        coins.callAll('animations.play', 'animations', 'shine');
+        
+        g = this.add.group();
+        g.enableBody = true;
+        map.createFromObjects('coins', 'finish', null, 0, true, false, g);
+        
+        
         group = this.add.group();
-    
         frog = group.create(65, 40, "frog");
         this.physics.enable(frog, Phaser.Physics.ARCADE);
         frog.anchor.setTo(0.5, 0.5);
@@ -36,14 +58,41 @@ function gameState() {
         frog.animations.add("jump", [0, 1], 5, true);
         
         this.camera.follow(frog);
-    
+        
+        coinCount = this.add.text(5, 5, "$ " + coinsCollected);
+        coinCount.fontSize = 20;
+        coinCount.fixedToCamera = true;
+        coinCount.fill = "#FFFFFF";
+        coinCount.stroke = '#000000';
+        coinCount.strokeThickness = 3;
+
+
+        timerText = this.add.text(this.camera.width - 70, 5, "0.0");
+        timerText.fontSize = 20;
+        timerText.fixedToCamera = true;
+        timerText.fill = "#FFFFFF";
+        timerText.stroke = '#000000';
+        timerText.strokeThickness = 3;
+
         cursors = this.input.keyboard.createCursorKeys();
     
     }
     
     function update() {
         
+        if (elapsedTime !== null) {
+            return;
+        } else if (startTime !== null) {
+            timerText.text = (((new Date()) - startTime) / 1000);
+        }
+        
         this.physics.arcade.collide(frog, layer);
+        this.physics.arcade.overlap(frog, coins, collectCoin, null, this);
+        this.physics.arcade.overlap(frog, g, function(f, g) {
+            frog.animations.stop(null, true);
+            elapsedTime = (new Date()) - startTime;
+            console.log("finished in " + (elapsedTime / 1000) + " seconds!");
+        }, null, this);
         
 		if (cursors.left.isDown) {
 			frog.body.velocity.x = -100;
@@ -70,9 +119,18 @@ function gameState() {
 		}
 		
 		if (frog.body.velocity.x != 0 || frog.body.velocity.y != 0) {
+		    if (startTime === null) {
+		        startTime = new Date();
+		    }
 			frog.animations.play("jump");
 		} else {
 			frog.animations.stop(null, true);
+		}
+		
+		if (elapsedTime !== null) {
+            frog.body.velocity.x = 0;
+            frog.body.velocity.y = 0;
+		    frog.animations.stop(null, true);
 		}
         
     }
@@ -90,11 +148,12 @@ function gameState() {
 	
 }
 
-width = 450;
-height = 280;
 
 $(function() {
 	
+    var width = $('#sizer').width();
+    var height = $('#cell-2').height();
+    
 	var state = gameState();
 	
 	var game = new Phaser.Game(width, height, Phaser.CANVAS, "cell-2",
