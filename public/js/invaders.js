c6c2e3c9caa0ecd339fbd99189d1013ba89375e7
Casey Leonard <casey@amphibian.com>
2016-05-18 21:53:19 -0400

    more work on space invaders game

--- public/js/invaders.js
+++ public/js/invaders.js
@@ -8,6 +8,8 @@ function gameState() {
     var cursors;
     var fireButton;
     var firingTimer = 0;
+    var aliens;
+    var alientTween;
 	
     function preload() {
 
@@ -19,7 +21,9 @@ function gameState() {
         
         this.physics.startSystem(Phaser.Physics.ARCADE);
         
-        player = this.add.sprite(200, 250, 'frog');
+        this.add.image(-10, 220, 'ground');
+        
+        player = this.add.sprite(200, 260, 'frog');
         player.anchor.setTo(0.5, 0.5);
         this.physics.enable(player, Phaser.Physics.ARCADE);
         
@@ -33,10 +37,42 @@ function gameState() {
         rocks.setAll('outOfBoundsKill', true);
         rocks.setAll('checkWorldBounds', true);
         
+        aliens = this.add.group();
+        aliens.enableBody = true;
+        aliens.physicsBodyType = Phaser.Physics.ARCADE;
+
+        createAliens();
+        
         
         cursors = this.input.keyboard.createCursorKeys();
         fireButton = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         
+    }
+    
+    function createAliens(lvl) {
+    	
+
+    	
+    	for (var y = 0; y < 4; y++)
+        {
+            for (var x = 0; x < 8; x++)
+            {
+                var alien = aliens.create(x * 48, y * 38, 'invader');
+                alien.anchor.setTo(0.5, 0.5);
+                alien.animations.add('fly', [ 0, 1 ], 2, true);
+                alien.play('fly');
+                alien.body.moves = false;
+            }
+        }
+
+        aliens.x = 10;
+        aliens.y = 5;
+
+        alientTween = game.add.tween(aliens).to( { x: 100 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
+        alientTween.onLoop.add(function() {
+        	aliens.y += 10;
+        }, game);
+    	
     }
     
     function update() {
@@ -51,18 +87,20 @@ function gameState() {
             } else if (cursors.right.isDown) {
                 player.body.velocity.x = 200;
             }
+            
+            if (player.body.x < 0) {
+            	player.body.x = 0;
+            }
+            
+            if (player.body.x > (this.world.width - player.body.width)) {
+            	player.body.x = (this.world.width - player.body.width);
+            }
 
             if (fireButton.isDown) {
                 throwRock(this);
             }
 
-            if (this.time.now > firingTimer)
-            {
-                //enemyFires();
-            }
-
-            //  Run collision
-            //this.physics.arcade.overlap(bullets, aliens, collisionHandler, null, this);
+            this.physics.arcade.overlap(rocks, aliens, collisionHandler, null, this);
             //this.physics.arcade.overlap(enemyBullets, player, enemyHitsPlayer, null, this);
             
         }
@@ -86,6 +124,22 @@ function gameState() {
 
     }
     
+    function collisionHandler (rock, alien) {
+
+        rock.kill();
+        alien.kill();
+        if (aliens.countLiving() == 0) {
+            this.input.onTap.addOnce(restart,this);
+        }
+
+    }
+    
+    function restart () {
+        aliens.removeAll();
+        alientTween.stop();
+        createAliens();
+    }
+    
     return {
         preload: preload,
         create: create,
@@ -95,7 +149,7 @@ function gameState() {
 	
 }
 
-
+var game;
 $(function() {
 	
     var width = $('#sizer').width();
@@ -103,7 +157,7 @@ $(function() {
     
 	var state = gameState();
 	
-	var game = new Phaser.Game(width, height, Phaser.CANVAS, "cell-1",
+	game = new Phaser.Game(width, height, Phaser.CANVAS, "cell-1",
 			state);
 	
 });
