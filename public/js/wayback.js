583f7cc68fe470ad7ab1906c519d22afc25b90d6
Casey Leonard <casey@amphibian.com>
2016-10-16 20:34:14 -0400

    wayback wednesday

---
+++ public/js/wayback.js
@@ -0,0 +1,9 @@
+$(function() {
+
+    $("#tm-4").click(function() {
+        
+        window.location = "https://web.archive.org/web/20021119214811/http://www.amphibian.com/";
+        
+    });
+    
+});
