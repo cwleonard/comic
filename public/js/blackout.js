9a3a2665713722451bc10e05c4778f9013283f55
Casey Leonard <casey@amphibian.com>
2016-11-24 18:19:21 -0500

    makes everything turn black

---
+++ public/js/blackout.js
@@ -0,0 +1,11 @@
+$(function() {
+	
+	$("body").css("background-color", "black");
+	$("header").css("background", "black");
+	$("footer").css("background", "black");
+	$(".ptop").css("background", "black");
+	$("div.holiday").hide();
+	$("div.box").css("box-shadow", "none");
+	$("div.box").css("border", "1px solid #333333");
+	
+});
