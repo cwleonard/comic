adecafd0934e373e8e099f4e7a6873fe345fcd3b
Casey Leonard <casey@amphibian.com>
2016-11-24 18:50:50 -0500

    black to red on scroll

--- public/js/blackout.js
+++ public/js/blackout.js
@@ -8,4 +8,28 @@ $(function() {
 	$("div.box").css("box-shadow", "none");
 	$("div.box").css("border", "1px solid #333333");
 	
+	
+	$(window).scroll(function() {
+		
+		if ($(this).scrollTop() > 600) {
+			
+			$("body").css("background-color", "red");
+			$("header").css("background", "red");
+			$("footer").css("background", "red");
+			$(".ptop").css("background", "red");
+			$("#cell-3").css("background", "red");
+			
+		} else {
+			
+			$("body").css("background-color", "black");
+			$("header").css("background", "black");
+			$("footer").css("background", "black");
+			$(".ptop").css("background", "black");
+			$("#cell-3").css("background", "black");
+			
+		}
+		
+	});
+
+	
 });
