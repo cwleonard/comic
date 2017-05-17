7c2e52672f21701080c5546471ec669c727b12e9
Casey Leonard <casey@amphibian.com>
2017-05-16 22:44:44 -0400

    rubber duck sounds

---
+++ public/js/rubberduck.js
@@ -0,0 +1,55 @@
+var sound1;
+var sound2;
+var sound3;
+var sound4;
+
+var sounds = [ sound1, sound2, sound3, sound4 ];
+
+$(function() {
+	
+	$('#rubber-duck').unbind('click'); // in case this gets executed more than once
+	
+	$('#rubber-duck').click(function() {
+	    
+	    var s = Math.floor(Math.random() * sounds.length);
+
+	    var playSound = sounds[s];
+	    
+	    if (playSound) {
+	        playSound.play();
+	    }
+	    
+	});
+
+	soundManager.setup({
+        url: '/swf/',
+        onready: function() {
+
+          sound1 = soundManager.createSound({
+            id: 'sound1',
+            url: '/audio/car_horn.mp3'
+          });
+          
+          sound2 = soundManager.createSound({
+              id: 'sound2',
+              url: '/audio/drill.mp3'
+            });
+          
+          sound3 = soundManager.createSound({
+              id: 'sound3',
+              url: '/audio/metal_hit.mp3'
+            });
+
+          sound4 = soundManager.createSound({
+              id: 'sound4',
+              url: '/audio/phone_ring.mp3'
+            });
+
+        },
+        ontimeout: function() {
+          // Hrmm, SM2 could not start. Missing SWF? Flash blocked? Show an error, etc.?
+            console.log("could not start soundmanager!");
+        }
+      }); 
+	   
+});
