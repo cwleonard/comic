71c40e126e15626d188f41d9a5e697fd74edfe0d
Casey Leonard <casey@amphibian.com>
2022-12-19 21:03:34 -0500

    background animation javascript

---
+++ public/js/shortest-day.js
@@ -0,0 +1,18 @@
+
+$(function() {
+
+    const css = window.document.styleSheets[0];
+
+    css.insertRule(`
+    @keyframes darken {
+    0% { background-color: #D3EEFF; }
+    100% { background-color: #000000; }
+    }`, css.cssRules.length);    
+
+    $("#cell-0").css({
+        "animation-name": "darken",
+        "animation-duration": "15s"
+    });
+
+});
+
